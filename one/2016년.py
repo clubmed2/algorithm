7763431ef3cdfc7a54efb3a37e968eaa29764652
clubmed2 <42707134+clubmed2@users.noreply.github.com>
2020-09-04 18:07:21 +0900

    algorithm

--- one/2016년.py
+++ one/2016년.py
@@ -2,13 +2,14 @@ from datetime import datetime
 
 
 def solution(a, b):
-	week = "FRI,SAT,SUN,MON,TUE,WED,THU".split(',')
-    day1 = datetime(2016,1,1)
-    day2 = datetime(2016,a,b)
+    week = "FRI,SAT,SUN,MON,TUE,WED,THU".split(',')
+    day1 = datetime(2016, 1, 1)
+    day2 = datetime(2016, a, b)
     return week[(day2-day1).days % 7]
+
 
 # 새로 배운 점
 '''
 1. 문제에 문자열이 주어졌을 때 split을 사용하여 리스트로 만들면 빠르다.
 2. datetime(2016,1,1).weekday()하면 요일을 구할 수 있다.
-'''        
+'''
